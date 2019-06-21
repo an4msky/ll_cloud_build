59388ea2156ed17ff5946b1816954b394b804850
an4msky <51996257+an4msky@users.noreply.github.com>
2019-06-21 17:03:36 +0300

    fix14

--- start_llr.sh
+++ start_llr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /opt/learninglocker/
-sudo -u docker pm2 start pm2/all.json --hp /home/docker
+pm2 start pm2/all.json --hp /home/docker
 cd /opt/xapi-service/
-sudo -u docker pm2 start pm2/xapi.json --hp /home/docker
+pm2 start pm2/xapi.json --hp /home/docker
 pm2 logs
