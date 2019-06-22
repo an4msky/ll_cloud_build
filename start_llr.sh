d0a5146d892722438b0335f6a9bf072c685752a4
an4msky <51996257+an4msky@users.noreply.github.com>
2019-06-22 13:29:39 +0300

    Update start_llr.sh

--- start_llr.sh
+++ start_llr.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-cd /opt/learninglocker/
-pm2 start pm2/all.json
-cd /opt/xapi-service/
-pm2 start pm2/xapi.json 
-pm2 logs
+#cd /opt/learninglocker/
+#pm2 start pm2/all.json
+#cd /opt/xapi-service/
+#pm2 start pm2/xapi.json 
+#pm2 logs
+
+cd /opt/learninglocker
+node worker/dist/server/ &
+node ui/dist/server/ &
+node api/dist/server/ &
