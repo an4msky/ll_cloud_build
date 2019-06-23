025c7f9e0088aeb847a8541a763f39d4d2dd6e82
an4msky <51996257+an4msky@users.noreply.github.com>
2019-06-23 13:16:31 +0300

    start pm2-docker only llr

--- start_llr.sh
+++ start_llr.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-#cd /opt/learninglocker/
-#pm2 start pm2/all.json
+cd /opt/learninglocker/
+pm2-docker start pm2/all.json
 #cd /opt/xapi-service/
 #pm2 start pm2/xapi.json 
-#pm2 logs
+pm2-docker logs
 
-cd /opt/learninglocker
+#cd /opt/learninglocker
 
-node worker/dist/server/ &
-node ui/dist/server/ &
-node api/dist/server/ &
-
-wait
+#node worker/dist/server/ &
+#node ui/dist/server/ &
+#node api/dist/server/ &
+#wait
